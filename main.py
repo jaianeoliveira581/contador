45e02736d2ba12d92fc6a7f7262819f3c94ba3a0
jaianeoliveira581 <jaianeoliveira947@gmail.com>
2024-05-21 21:06:44 -0300

    versão 1.1

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 
+import os
+import time
+
 contador = int(input('informe um numero inteiro: '))
+os.system('cls')
 
 
 while contador >= 0:
-    print(contador)
+    print(f'contagem regressiva: {contador}:...')
+    time.sleep(0.1)
+    os.system('cls')
     contador -= 1
+
+print('BOOM!!!')
